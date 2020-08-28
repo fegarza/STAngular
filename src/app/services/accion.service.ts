edd440b5320445cecd64f96549fcff5f82f64218
Felipe Garza <fegarza@outlook.es>
2020-08-27 19:13:47 -0500

    nueva configuracion de api

--- src/app/services/accion.service.ts
+++ src/app/services/accion.service.ts
@@ -8,6 +8,8 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 
+Hola
+
 export class AccionService {
 
   constructor(private constants: ConstantsService, private http: HttpClient) {}
